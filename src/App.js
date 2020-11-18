db21318b6783b4730baf80210359291d6260b391
chikechris <cchike2017@gmail.com>
2020-11-18 07:11:12 -0500

    added styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-
+  import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
 function App() {
+  const [questions, setQuestions] = useState(data);
   return (
-    <div className="App">
-    Accordion App
-    </div>
+    <main>
+      <div className='container'>
+        <h3>questions and answers about login</h3>
+        <section className='info'>
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}></SingleQuestion>
+            );
+          })}
+        </section>
+      </div>
+    </main>
   );
 }
 
